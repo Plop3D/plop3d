c52b133891de556f5bfb506812e5a894dce64b0a
Nick Petrus <nick@atlas.co>
2017-02-25 14:46:09 -0800

    Add shape

--- src/systems/brush.js
+++ src/systems/brush.js
@@ -201,6 +201,16 @@ AFRAME.registerSystem('brush', {
       }
     }
   },
+  addShape: function(brushName) {
+    var brush = document.querySelector('#left-hand').components.brush;
+    var currentStroke = this.addNewStroke(brush.data.brush, brush.color, 1);
+    var position = new THREE.Vector3();
+    var rotation = new THREE.Quaternion();
+    var scale = new THREE.Vector3();
+    brush.obj.matrixWorld.decompose(position, rotation, scale);
+    var pointerPosition = this.getPointerPosition(position, rotation);
+    currentStroke.addPoint(position, rotation, pointerPosition, brush.sizeModifier, Date.now);
+  },
   addNewStroke: function (brushName, color, size) {
     var Brush = this.getBrushByName(brushName);
     if (!Brush) {
