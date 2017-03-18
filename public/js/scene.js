4f0cbeed720d901b1331ad622e725ac8f3d7b55d
Nick Petrus <nick@atlas.co>
2017-03-18 14:33:09 -0700

    Paints on scene not camera

--- public/js/scene.js
+++ public/js/scene.js
@@ -1,50 +1,51 @@
 var scene, camera
-Cute.ready(function () {
+Cute.ready(function() {
   scene = Cute.one('a-scene')
   camera = Cute.one('a-camera')
 })
 
-Cute.on('touchend', function () {
+Cute.on('touchend', function() {
   scene.enterVR()
   socket.emit('mode', 'vr')
 })
 
-socket.on('phone', function (data) {
-  Cute.all('#' + data.phone + '-phone *', function (tag) {
+socket.on('phone', function(data) {
+  Cute.all('#' + data.phone + '-phone *', function(tag) {
     Cute.attr(tag, 'opacity', 1)
   })
 })
 
-socket.on('tilt', function (data) {
+socket.on('tilt', function(data) {
   var phone = Cute.one('#' + data.phone + '-phone')
   var rotation = data.beta + ' ' + data.alpha + ' ' + -data.gamma
   Cute.attr(phone, 'rotation', rotation)
 })
 
-socket.on('acceleration', function (data) {
+socket.on('acceleration', function(data) {
   var phone = Cute.one('#' + data.phone + '-phone')
   var position = data.x + ' ' + data.y + ' ' + data.z
 })
 
-socket.on('finger', function (data) {
+socket.on('finger', function(data) {
   var finger = Cute.one('#' + data.name + '-finger')
   var position = data.x + ' ' + data.y + ' ' + data.z
   Cute.attr(finger, 'position', position)
 })
 
 var last
-socket.on('draw:start', function (data) {
+socket.on('draw:start', function(data) {
   last = data
   Cute.all('a-cylinder.draw-stroke', Cute.remove)
 })
 
 var drawN = 0
-socket.on('draw:move', function (data) {
-  var position = [
+socket.on('draw:move', function(data) {
+  var pos = camera.object3D.localToWorld(new THREE.Vector3(
     (data.x + last.x) / 2,
     (data.y + last.y) / 2,
-    (data.z + last.z) / 2
-  ]
+    (data.z + last.z) / 2));
+
+  var position = [pos.x, pos.y, pos.z]
   var dx = data.x - last.x
   var dy = data.y - last.y
   var dz = data.z - last.z
@@ -62,10 +63,10 @@ socket.on('draw:move', function (data) {
   var rotation = [a, b, 0]
   var radius = 0.01
 
-  Cute.add(camera, 'a-cylinder.draw-stroke?color=#ff0&radius=' + radius + '&segments-radial=6&height=' + (height + radius / 2) + '&position=' + position.join(' ') + '&rotation=' + rotation.join(' '))
+  Cute.add(scene, 'a-cylinder.draw-stroke?color=#ff0&radius=' + radius + '&segments-radial=6&height=' + (height + radius / 2) + '&position=' + position.join(' ') + '&rotation=' + rotation.join(' '))
 
   last = data
 })
 
-socket.on('draw:end', function (data) {
+socket.on('draw:end', function(data) {
 })
